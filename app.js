2f812c9c53a7dadd1e75faf4470dc634b1c4375f
Abhay <chauhan27.abhay@gmail.com>
2021-01-18 15:25:09 +0530

    introduced sessions

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ require('dotenv').config({path: './config/config.env'})
 const express = require("express")
 const bodyParser = require("body-parser");
 const connectDB = require('./config/db')
+const session = require('express-session');
+const passport = require('passport');
 
 const app = express();
 
@@ -10,6 +12,17 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
+// session
+app.use(session({
+  secret: 'my little secret',
+  resave: false,
+  saveUninitialized: false
+}))
+
+// passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
 
 connectDB();
 
